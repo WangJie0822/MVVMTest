de4766c7b350f4126386e0ca6f9d910395853d0b
王杰 <wangjie@zanchina.com>
2018-07-19 19:29:23 +0800

    创建项目

---
+++ app/src/main/kotlin/cn/wj/android/mvvm_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.wj.android.mvvm_test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_activity_main)
+    }
+}
